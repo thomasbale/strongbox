0f456b2b33723aba9f7625126ac59bb0ae44f644
Thomas Bale <thomasbale@gmail.com>
2018-04-29 15:48:06 +0100

    Basic choicebox functioning
    Implimented basic choice box functionality which will allow users to choose an encryption method

--- src/strong/box/FXMLDocumentController.java
+++ src/strong/box/FXMLDocumentController.java
@@ -6,98 +6,115 @@
 package strong.box;
 
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import javafx.stage.*;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.BadPaddingException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
+import javafx.scene.control.ChoiceBox;
 
 /**
- *
+ * This class controls the application layer (model) sitting behind the GUI
  * @author thomasbale
  */
 public class FXMLDocumentController implements Initializable {
+    //  Selected file needs to be visible to whole of controller
+    File selectedFile;
+            
+    @FXML private Label status;
+    @FXML private Label instruction;
+    @FXML private TextField key;
+    @FXML private ChoiceBox method = new ChoiceBox();
     
-    @FXML
-    private Label label;
-    private Label label2;
-
+    ObservableList<String> options = 
+    FXCollections.observableArrayList(
+        "Option 1",
+        "Option 2",
+        "Option 3"
+    );
+    
+    public FXMLDocumentController() {
+        
+    }
     @FXML
     private void handleUploadedFile(ActionEvent event) {
-        
+         
         System.out.println("File submitted");
         
         FileChooser fileChooser = new FileChooser();
-        File selectedFile = fileChooser.showOpenDialog(null);
-        
+        //  Launches a dialogue window to the main scene
+        selectedFile = fileChooser.showOpenDialog(null);
+        //  Handle cases where the user aborts or the process fails
         if (selectedFile != null) {
 
-                label.setText("File selected: " + selectedFile.getName());
-                startProcess(selectedFile);
+                status.setText("File selected: " + selectedFile.getName());
+                
             }
             else {
-                label.setText("File selection cancelled.");
+                status.setText("File selection cancelled.");
             }
     }
     
     @FXML
     private void encryptFile(ActionEvent event) {
         
-        label.setText("Encrypting file... " );
+        String key = "This is a secret";
+        startProcess(selectedFile, key,true);
+        try{
+            TimeUnit.SECONDS.sleep(1);
+        } catch(InterruptedException e){}
+        status.setText("Encryption complete " );
     }
     
     @FXML
     private void decryptFile(ActionEvent event) {
         
-        label.setText("Decrypting file..." );
-      
+        String key1 = "This is a secret";
+        startProcess(selectedFile, key1,false);
+        try{
+            TimeUnit.SECONDS.sleep(1);
+        } catch(InterruptedException e){};
+        status.setText("Decryption complete " );
+        status.setText(key.getText());
     }
     
-    private void startProcess(File input) {
+    private void startProcess(File input, String key, boolean mode) {
         
-  
-        String key = "This is a secret";
-  
         File encryptedFile = new File("text.encrypted");
         File decryptedFile = new File("decrypted-text.txt");
+        comboBoxwasUpdated();
 
-  try {
-    Crypto.fileProcessor(Cipher.ENCRYPT_MODE,key,input,encryptedFile);
-    Crypto.fileProcessor(Cipher.DECRYPT_MODE,key,encryptedFile,decryptedFile);
-    System.out.println("Success");
-  }
+        try {
+            if(mode = true){
+                Crypto.fileProcessor(Cipher.ENCRYPT_MODE,key,input,encryptedFile);
+            }else{
+                Crypto.fileProcessor(Cipher.DECRYPT_MODE,key,encryptedFile,decryptedFile);
+            }
+          System.out.println("Success");
+        }
 
-  catch (Exception ex) {
-    System.out.println(ex.getMessage());
-          ex.printStackTrace();
-  
-}
-        
-      
+        catch (Exception ex) {
+          System.out.println(ex.getMessage());
+        }     
  }
     
+    //This wil update the combobox when the combobox is changed
+    public void comboBoxwasUpdated(){
+       this.instruction.setText(method.getValue().toString());
+    }
     
-    
-    
-    
+    // Function gets called at GUI start
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    method.setItems(options);
+         
     }    
     
 }
